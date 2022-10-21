45ac1ee3cce9b3313304eceb74f54a3022bcaeb5
atamayo1 <anthony1_144@hotmail.com>
2022-10-20 23:41:33 -0500

    i add the menu for the routes

--- src/routers/RouterPrincipal.js
+++ src/routers/RouterPrincipal.js
@@ -1,22 +1,44 @@
-import React from 'react';
-import { Routes, Route, Link, BrowserRouter } from 'react-router-dom';
-import { Home } from '../components/Home';
-import { Articles } from '../components/Articles';
-import { Contact } from '../components/Contact';
-import { NotFound } from '../components/NotFound';
+import React from "react";
+import { Routes, Route, NavLink, BrowserRouter } from "react-router-dom";
+import { Home } from "../components/Home";
+import { Articles } from "../components/Articles";
+import { Contact } from "../components/Contact";
+import { NotFound } from "../components/NotFound";
 
 const RouterPrincipal = () => {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home />}></Route>
-                <Route path="/home" element={<Home />}></Route>
-                <Route path="/articles" element={<Articles />}></Route>
-                <Route path="/contact" element={<Contact />}></Route>
-                <Route path="*" element={<NotFound />}></Route>
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <h1>Header</h1>
+      <hr />
+      <nav>
+        <ul>
+          <li>
+            <NavLink to={"/home"}>Inicio</NavLink>
+          </li>
+          <li>
+            <NavLink to={"/articles"}>Articles</NavLink>
+          </li>
+          <li>
+            <NavLink to={"/contact"}>Contact</NavLink>
+          </li>
+        </ul>
+      </nav>
+      <hr />
+      <section>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/home" element={<Home />}></Route>
+          <Route path="/articles" element={<Articles />}></Route>
+          <Route path="/contact" element={<Contact />}></Route>
+          <Route path="*" element={<NotFound />}></Route>
+        </Routes>
+      </section>
+      <hr />
+      <footer>
+        <p>footer</p>
+      </footer>
+    </BrowserRouter>
+  );
 };
 
 export default RouterPrincipal;
